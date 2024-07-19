3070675bf707cead27951ac56c84639f83bcb186
tufailzaman <tufailzaman789@gmail.com>
2024-07-19 16:37:06 +0500

    last email added

--- controller/user.js
+++ controller/user.js
@@ -50,74 +50,6 @@ exports.postUser = async (req, res, next) => {
             subject: "Verfy your email",
             html: `<h1> your verificetion code is ${verificationCode}. </h1>`
         })
-    //     transporter.sendMail({
-    //         to: email,
-    //         from: 'tufailzaman789@gmail.com',
-    //         subject: 'Welcomed to Sign up page',
-    //         html: `<html> 
-    //                      <head>
-    //                      <style>
-    // body {
-    //         font-family: Arial, sans-serif;
-    //         background-color: #f5f5f5;
-    //     }
-    
-    
-    // .container {
-    //         max-width: 600px;
-    //         margin: 40px auto;
-    //         background-color: #fff;
-    //         padding: 20px;
-    //         border: 1px solid #ddd;
-    //         border-radius: 10px;
-    //         box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    //     }
-        
-    //     .logo {
-    //         font-size: 24px;
-    //         font-weight: bold;
-    //         color: #333;
-    //         text-decoration: none;
-    //     }
-        
-    //     .button {
-    //         background-color: #4CAF50;
-    //         color: #fff;
-    //         padding: 10px 20px;
-    //         border: none;
-    //         border-radius: 5px;
-    //         cursor: pointer;
-    //     }
-        
-    //     .button:hover {
-    //         background-color: #3e8e41;
-    //     }
-    //                      </style>
-    //                      </head>
-    //                      <body>
-    //                      <div class="container">
-    
-    
-    // <h2>Welcome to Sign up, ${firstName} ${lastName}!</h2>
-    
-    
-    // <p>Thank you for signing up for our Sign Up Page! We're excited to help you take your career to the next level.</p>
-    
-    
-    // <p>With our cutting-edge technology, you'll be able to create a professional CV in minutes and apply to your dream jobs with ease. Our system is designed to help you stand out from the competition and increase your chances of getting hired.</p>
-    
-    
-    // <p>If you have any questions or need assistance, please don't hesitate to reach out to us at tufailzaman789@gmail.com. We're here to help.</p>
-    
-    
-    // <p>Best regards,</p>
-    // <p>Automated Cv genration and apply system </p>
-    
-    //     </div>
-    //     </body>
-                         
-    //              </html>`
-    //       });
         res.send({
             sucess: true,
             error: false,
@@ -145,6 +77,74 @@ exports.verifyUser = async (req, res, next) => {
     user.verificationCode = "";
 
      await user.save();
+           transporter.sendMail({
+            to: email,
+            from: 'tufailzaman789@gmail.com',
+            subject: 'Welcomed to Sign up page',
+            html: `<html> 
+                         <head>
+                         <style>
+    body {
+            font-family: Arial, sans-serif;
+            background-color: #f5f5f5;
+        }
+    
+    
+    .container {
+            max-width: 600px;
+            margin: 40px auto;
+            background-color: #fff;
+            padding: 20px;
+            border: 1px solid #ddd;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        
+        .logo {
+            font-size: 24px;
+            font-weight: bold;
+            color: #333;
+            text-decoration: none;
+        }
+        
+        .button {
+            background-color: #4CAF50;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        
+        .button:hover {
+            background-color: #3e8e41;
+        }
+                         </style>
+                         </head>
+                         <body>
+                         <div class="container">
+    
+    
+    <h2>Welcome to Sign up, ${firstName} ${lastName}!</h2>
+    
+    
+    <p>Thank you for signing up for our Sign Up Page! We're excited to help you take your career to the next level.</p>
+    
+    
+    <p>With our cutting-edge technology, you'll be able to create a professional CV in minutes and apply to your dream jobs with ease. Our system is designed to help you stand out from the competition and increase your chances of getting hired.</p>
+    
+    
+    <p>If you have any questions or need assistance, please don't hesitate to reach out to us at tufailzaman789@gmail.com. We're here to help.</p>
+    
+    
+    <p>Best regards,</p>
+    <p>Automated Cv genration and apply system </p>
+    
+        </div>
+        </body>
+                         
+                 </html>`
+          });
      res.send({
         sucess: true,
         error: false,
